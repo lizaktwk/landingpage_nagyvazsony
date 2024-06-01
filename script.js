39047301c4eac0e505300a7e81407dd857f04b5e
lizaktwk <101625207+lizaktwk@users.noreply.github.com>
2024-06-02 00:50:43 +0200

    Welcome to Nagyvázsony
    - Div appearance at specific background image
    - responsive behaviour for welcome

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-// Creating the BG image sequence (array)
 function imageCache(base, firstNum, lastNum) {
     this.cache = [];
-    let frames;
     for (let i = firstNum; i <= lastNum; i++) {
-        frames = new Image();
+        const frames = new Image();
         if (i <= 9) {
             frames.src = base + "00" + i + ".png";
         } else if (i <= 99) {
@@ -15,53 +13,70 @@ function imageCache(base, firstNum, lastNum) {
     }
 }
 
-let myCache = new imageCache('./Test-Video-Web-PNGs/Test-Video', 0, 479);
-
-// Set the height of the document based on the number of images
-document.body.style.height = `${(myCache.cache.length - 1) * window.innerHeight}px`;
-
-// Variable to keep track of the current image index
+const myCache = new imageCache('./Test-Video-Web-PNGs/Test-Video', 0, 479);
 let currentImageIndex = 0;
 
-// Function to update the displayed image
+// Calculate the height of the document based on the number of images
+document.body.style.height = `${(myCache.cache.length - 1) * window.innerHeight}px`;
+
+document.getElementById("table-content").style.visibility = "hidden";
+
 function updateImage() {
-    // Check if the current index is within bounds
     if (currentImageIndex >= 0 && currentImageIndex < myCache.cache.length) {
-        let currentImage = myCache.cache[currentImageIndex];
-        let mainImg = document.getElementById('mainImg');
-        // Display the image (replace 'imageContainer' with your image container's ID or class)
+        const currentImage = myCache.cache[currentImageIndex];
+        const mainImg = document.getElementById('mainImg');
         mainImg.src = currentImage.src;
-        // Set the z-index of the currently displayed image
         mainImg.style.zIndex = 0;
+
+        console.log("current Index: " + currentImageIndex);
+
+        // show welcome table content at specific image index
+        if(currentImageIndex >= 129 && currentImageIndex <= 131) {
+            showTableContent('welcome', 129, 130);
+            console.log("welcome displayed");
+        } else {
+            hideTableContent('welcome');
+        }
     }
 }
 
-// Implementing backwards/forwards animation based on scroll direction
 window.addEventListener('scroll', function () {
-    // Calculating the maximum vertical distance that can be scrolled
-    let maxScrollTop = document.body.scrollHeight - window.innerHeight; // total document height - visible area height
-    // Calculate the fraction of the scroll position
-    let scrollFraction = window.scrollY / maxScrollTop; // represents how much of the scrollable distance has been scrolled (value between 0 and 1)
-    
-    // Calculate the new image index with finer granularity
-    let newIndex = Math.min(
-        myCache.cache.length - 1,
-        Math.floor(scrollFraction * myCache.cache.length)
-    );
+    const maxScrollTop = document.body.scrollHeight - window.innerHeight;
+    const scrollFraction = window.scrollY / maxScrollTop;
+    const newIndex = Math.min(myCache.cache.length - 1, Math.floor(scrollFraction * myCache.cache.length));
 
-    // Only update if the index has changed
     if (newIndex !== currentImageIndex) {
         currentImageIndex = newIndex;
-        // print the current image index
         console.log(currentImageIndex);
         updateImage();
+        setZIndex();
     }
 });
 
-// reset animation and scroll-position on reload
 window.onbeforeunload = function () {
     window.scrollTo(0, 0);
 };
 
+function setZIndex() {
+    const allElements = document.getElementsByTagName('*');
+    
+    for (let i = 0; i < allElements.length; i++) {
+        const element = allElements[i];
+        if (element.id !== 'mainImg' && element.className !== 'icon') {
+            element.style.zIndex = 1;
+            console.log("element z-index: " + element.style.zIndex);
+        }
+    }
+};
 
+// display div with id when specific mainImg is displayed
+function showTableContent(id, startImg, endImg) {
+    if (currentImageIndex >= startImg && currentImageIndex <= endImg) {
+        document.getElementById(id).style.visibility = "visible";
 
+    }
+}
+
+function hideTableContent(id) {
+    document.getElementById(id).style.visibility = 'hidden';
+}
--- testscript.js
+++ testscript.js
@@ -1,9 +1,7 @@
-// Creating the BG image sequence (array)
 function imageCache(base, firstNum, lastNum) {
     this.cache = [];
-    let frames;
     for (let i = firstNum; i <= lastNum; i++) {
-        frames = new Image();
+        const frames = new Image();
         if (i <= 9) {
             frames.src = base + "00" + i + ".png";
         } else if (i <= 99) {
@@ -15,60 +13,83 @@ function imageCache(base, firstNum, lastNum) {
     }
 }
 
-let myCache = new imageCache('./Test-Video-Web-PNGs/Test-Video', 0, 479);
-
-// Set the height of the document based on the number of images
-document.body.style.height = `${(myCache.cache.length - 1) * window.innerHeight}px`;
-
-// Variable to keep track of the current image index
+const myCache = new imageCache('./Test-Video-Web-PNGs/Test-Video', 0, 479);
 let currentImageIndex = 0;
 
-// Function to update the displayed image
+// Calculate the height of the document based on the number of images
+document.body.style.height = `${(myCache.cache.length - 1) * window.innerHeight}px`;
+
 function updateImage() {
-    // Check if the current index is within bounds
     if (currentImageIndex >= 0 && currentImageIndex < myCache.cache.length) {
-        let currentImage = myCache.cache[currentImageIndex];
-        // Display the image (replace 'imageContainer' with your image container's ID or class)
-        document.getElementById('mainImg').src = currentImage.src;
+        const currentImage = myCache.cache[currentImageIndex];
+        const mainImg = document.getElementById('mainImg');
+        mainImg.src = currentImage.src;
+        mainImg.style.zIndex = 0;
+
+        console.log("current Index: " + currentImageIndex);
+
+        // show welcome table content at specific image index
+        if(currentImageIndex >= 129 && currentImageIndex <= 131) {
+            showTableContent('welcome');
+            showTableContent('welcome-text');
+        } else {
+            hideTableContent('welcome');
+            hideTableContent('welcome-text');
+        }
     }
 }
 
-// Implementing backwards/forwards animation based on scroll direction
 window.addEventListener('scroll', function () {
-    // Calculating the maximum vertical distance that can be scrolled
-    let maxScrollTop = document.body.scrollHeight - window.innerHeight; // total document height - visible area height
-    // Calculate the fraction of the scroll position
-    let scrollFraction = window.scrollY / maxScrollTop; // represents how much of the scrollable distance has been scrolled (value between 0 and 1)
-    
-    // Calculate the new image index with finer granularity
-    let newIndex = Math.min(
-        myCache.cache.length - 1,
-        Math.floor(scrollFraction * myCache.cache.length)
-    );
+    const maxScrollTop = document.body.scrollHeight - window.innerHeight;
+    const scrollFraction = window.scrollY / maxScrollTop;
+    const newIndex = Math.min(myCache.cache.length - 1, Math.floor(scrollFraction * myCache.cache.length));
 
-    // Only update if the index has changed
     if (newIndex !== currentImageIndex) {
         currentImageIndex = newIndex;
-        // print the current image index
         console.log(currentImageIndex);
         updateImage();
+        // setZIndex();
     }
 });
 
-// reset animation and scroll-position on reload
 window.onbeforeunload = function () {
     window.scrollTo(0, 0);
 };
 
+// function setZIndex() {
+//     const allElements = document.getElementsByTagName('*');
+    
+//     for (let i = 0; i < allElements.length; i++) {
+//         const element = allElements[i];
+//         if (element.id !== 'mainImg' && element.id !== 'icon-container') {
+//             element.style.zIndex = 1;
+//             console.log("element z-index: " + element.style.zIndex);
+//         }
+//     }
+// };
 
-function setZIndex() {
-    let iconContainer = document.getElementById('icon-container');
-    let icons = document.getElementsByClassName('icon');
-    for (let i = 0; i < icons.length; i++) {
-        icons[i].style.zIndex = '1';
-    }
+// display div with id when specific mainImg is displayed
+// function showTableContent(contentId) {
+//     const tableContent = document.getElementById('table-content');
+//     const children = tableContent.children;
+
+//     for (let i = 0; i < children.length; i++) {
+//         const child = children[i];
+//         if (child.id === contentId) {
+//             child.style.visibility = "visible";
+//         } else {
+//             child.style.visibility = "hidden";
+//         }
+//     }
+// }
+function showTableContent(contentId) {
+    let element = document.getElementById(contentId);
+    element.style.visibility = 'visible';
 }
 
-
-
-
+function hideTableContent(id) {
+    const element = document.getElementById(id);
+    if (element) {
+        element.style.visibility = 'hidden';
+    }
+}
